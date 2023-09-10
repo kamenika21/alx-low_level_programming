6868121ddca412648b3002f4c92cc83d6c65fcaa
Kamenika <lunarlegend21k@gmail.com>
2023-09-10 15:51:55 -0700

    Task 3

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Print alphabets lowercase and uppercase
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 97; i <= 122; i++)
+	{
+		putchar(i);
+	}
+
+	for (j = 65; j <= 90; j++)
+	{
+		putchar(j);
+	}
+
+	putchar('\n');
+	return (0);
+}
