31909737b3cd918cb7f4780e6d8c1d2c47860815
Kamenika <lunarlegend21k@gmail.com>
2023-09-10 14:11:12 -0700

    Task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -13,11 +13,11 @@ int main(void)
 	long long int lli;
 	float fl;
 
-	printf("Size of a char: %d byte(s)", sizeof(ch));
-	printf("Size of an int: %d byte(s)", sizeof(i));
-	printf("Size of a long int: %d byte(s)", sizeof(li));
-	printf("Size of a long long int: %d byte(s)", sizeof(lli));
-	printf("Size of a float: %d byte(s)", sizeof(fl));
+	printf("Size of a char: %d byte(s)\n", sizeof(ch));
+	printf("Size of an int: %d byte(s)\n", sizeof(i));
+	printf("Size of a long int: %d byte(s\n)", sizeof(li));
+	printf("Size of a long long int: %d byte(s)\n", sizeof(lli));
+	printf("Size of a float: %d byte(s)\n", sizeof(fl));
 
 	return (0);
 }
