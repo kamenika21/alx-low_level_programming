6615c41d52c126cb880e543a35dc07d072e76817
Kamenika <lunarlegend21k@gmail.com>
2023-09-14 04:11:06 -0700

    Debugging

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * main - check the function positive_or_negative
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative(i);
+
+	return (0);
+}
