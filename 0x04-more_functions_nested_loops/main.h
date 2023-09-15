73be01fd3f11c8f65ebcd0b6334f18771a89dfcd
Kamenika <lunarlegend21k@gmail.com>
2023-09-15 13:14:46 -0700

    Main.h

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -1,37 +1,17 @@
-#include <stdio.h>
-#include "main.h"
+#ifndef HEADER_FILE
+#define HEADER_FILE
 
-/**
- * print_remaining_days - takes a date and prints how many days are
- * left in the year, taking leap years into account
- * @month: month in number format
- * @day: day of month
- * @year: year
- * Return:: void
- */
+int _putchar(char c);
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+void print_number(int n);
 
-void print_remaining_days(int month, int day, int year)
-{
-	if (year % 4 == 0 || (year % 400 == 0 && year % 100 == 0))
-	{
-		if (month > 2 && day >= 60)
-		{
-			day++;
-		}
-
-		printf("Day of the year: %d\n", day);
-		printf("Remaining days: %d\n", 366 - day);
-	}
-	else
-	{
-		if (month == 2 && day == 60)
-		{
-			printf("Invalid date: %02d/%02d/%04d\n", month, day - 31, year);
-		}
-		else
-		{
-			printf("Day of the year: %d\n", day);
-			printf("Remaining days: %d\n", 365 - day);
-		}
-	}
-}
+#endif
