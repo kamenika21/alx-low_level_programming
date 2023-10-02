d266880c0577fa6d90f06c4e73b9e17e1b336c64
Kamenika <lunarlegend21k@gmail.com>
2023-10-02 08:19:17 -0700

    Task 8

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <main.h>
+
+/**
+ * print_square - print the square using the given number
+ * @s: The number to print the square of
+ */
+void print_square(int s)
+{
+	int i, j;
+
+	if (s > 0)
+	{
+		for (i = 0; i < s; i++)
+		{
+			for (j = 0; j < s; j++)
+			{
+				_putchar('#');
+			}
+
+			_putchar('\n');
+		}
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
